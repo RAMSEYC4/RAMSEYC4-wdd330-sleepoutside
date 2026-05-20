0109c9d27b0e0ff1b25fe3c8a9dff7b4c8a8e76e
RAMSEYC4 <ramseycedou68@gmail.com>
2026-05-20 15:00:54 +0200

    week 2 work

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,1 +1,40 @@
-//This script file will be programmed to contain the code to dynamically produce the product detail pages.
+import { setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addProductToCart.bind(this));
+  }
+
+  addProductToCart() {
+    setLocalStorage("so-cart", this.product);
+  }
+
+  renderProductDetails() {
+    const section = document.querySelector(".product-detail");
+    section.innerHTML = `
+      <h3>${this.product.Brand.Name}</h3>
+      <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+      <img
+        class="divider"
+        src="${this.product.Image}"
+        alt="${this.product.Name}"
+      />
+      <p class="product-card__price">$${this.product.FinalPrice}</p>
+      <p class="product__color">${this.product.Colors[0].ColorName}</p>
+      <p class="product__description">${this.product.DescriptionHtmlSimple}</p>
+      <div class="product-detail__add">
+        <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+      </div>
+    `;
+  }
+}
